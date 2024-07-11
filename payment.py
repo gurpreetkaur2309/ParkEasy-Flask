47819f363306ca1df8a3510b7fa75318629a33d5
codegeek004 <yashvaishnav2022@vitbhopal.ac.in>
2024-07-11 16:02:26 +0530

    Figured out but new issue raised

--- payment.py
+++ payment.py
@@ -70,7 +70,7 @@ def add_data():
             rate = 0
             if VehicleType in ['sedan', 'SUV', 'Hatchback', 'Coupe']:
                 rate = 13
-            elif VehicleType == '2-Wheeler':
+            elif VehicleType == '2-Wheeler'k:
                 rate = 8
             elif VehicleType == 'Heavy-Vehicle':
                 rate = 15
