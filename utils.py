3c785f8d8402aa3d6c98090115406383e86ff2ec
codegeek004 <yashvaishnav2022@vitbhopal.ac.in>
2024-10-03 11:39:55 +0530

    Issues resolved;

--- utils.py
+++ utils.py
@@ -1,14 +1,14 @@
 from functools import wraps
 from db import db, cursor
 from flask import  flash, redirect, url_for, flash
-from flask import  session as flask_session
+from flask import  session 
 
 def requires_role(roles):
     def decorator(view_func):
         @wraps(view_func)
         def wrapped_view(*args, **kwargs):
 
-            if flask_session.get('role') in roles:
+            if session.get('role') in roles:
                 return view_func(*args, **kwargs)
             else:
                 #flash('You are not authorized to access vehicle details','danger')
