78fad59db3177149e1998bfeb6b85a3968743bd7
gurpreetkaur2309 <128304835+gurpreetkaur2309@users.noreply.github.com>
2024-11-19 17:34:45 +0530

    Changed user, password and db name

--- db.py
+++ db.py
@@ -2,8 +2,8 @@ import mysql.connector
 db_config1 = {
         'host' : 'localhost',
         'user' : 'root',
-        'password' : 'root',
-        'database' : 'parking'
+        'password' : 'Root@123',
+        'database' : 'yashdb'
         }
 db = mysql.connector.connect(**db_config1)
 cursor = db.cursor(buffered=True)
